1895c6494d70b03a9dda1f2c531e81116126c469
skskdmsdl <skskdmsdl@gmail.com>
2022-05-08 21:18:20 +0900

    DDL 스크립트 수정

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `USERS` (
-	`id`	NUMBER¡¡ NOT¡¡NULL,
+	`user_id`	NUMBER¡¡ NOT¡¡NULL,
 	`email`	VARCHAR2(100) NOT¡¡NULL,
 	`password`	VARCHAR2(50)	NOT NULL,
 	`nickname`	VARCHAR2(50)	NOT NULL,
@@ -13,38 +13,38 @@ CREATE TABLE `USERS` (
 
 
 CREATE TABLE `LOGIN_LOG` (
-	`id`	NUMBER¡¡ NOT¡¡NULL,
+	`user_id`	NUMBER¡¡ NOT¡¡NULL,
 	`login_date`	DATE	 DEFAULT SYSDATE,
 	`login_status`	VARCHAR2(5),
-	CONSTRAINT pk_login_log  PRIMARY KEY (id, login_date),
-	CONSTRAINT fk_login_log FOREIGN KEY (id) REFERENCES USERS (id)
+	CONSTRAINT pk_login_log  PRIMARY KEY (user_id, login_date),
+	CONSTRAINT fk_login_log FOREIGN KEY (user_id) REFERENCES USERS (user_id)
 	--CONSTRAINT ck_login_status CHECK (¡®in¡¯, ¡®out¡¯),
 );
 
 CREATE TABLE `DIARY` (
-	`id`	NUMBER	NOT NULL,
+	`diary_id`	NUMBER	NOT NULL,
 	`user_id`	NUMBER	NOT NULL,
 	`title`	VARCHAR(255) NOT NULL,
 	`content`	VARCHAR(4000)	,
 	`create_date`	DATE	DEFAULT SYSDATE,
 	`diary_date`	DATE,
-	CONSTRAINT pk_diary PRIMARY KEY (id, user_id),
-	CONSTRAINT fk_diary FOREIGN KEY (user_id) REFERENCES USERS (id)
+	CONSTRAINT pk_diary PRIMARY KEY (diary_id, user_id),
+	CONSTRAINT fk_diary FOREIGN KEY (user_id) REFERENCES USERS (user_id)
 );
 
 CREATE TABLE `DIARY_IMAGE` (
-	`id`	NUMBER	NOT NULL,
+	`diary_id`	NUMBER	NOT NULL,
 	`real_name`	VARCHAR(255),
 	`file_name`	VARCHAR(255),
 	`path`	VARCHAR(255),
 	`create_date`	DATE	DEFAULT SYSDATE,
-	CONSTRAINT pk_like PRIMARY KEY (id),
-	CONSTRAINT fk_like FOREIGN KEY (id) REFERENCES DIARY (id)
+	CONSTRAINT pk_like PRIMARY KEY (diary_id),
+	CONSTRAINT fk_like FOREIGN KEY (diary_id) REFERENCES DIARY (diary_id)
 );
 
 CREATE TABLE `LIKE` (
-	`id`	NUMBER	NOT NULL,
+	`diary_id`	NUMBER	NOT NULL,
 	`create_date`	DATE	DEFAULT SYSDATE,
-	CONSTRAINT pk_like PRIMARY KEY (id),
-	CONSTRAINT fk_like FOREIGN KEY (id) REFERENCES DIARY (id)
+	CONSTRAINT pk_like PRIMARY KEY (diary_id),
+	CONSTRAINT fk_like FOREIGN KEY (diary_id) REFERENCES DIARY (diary_ids)
 );
