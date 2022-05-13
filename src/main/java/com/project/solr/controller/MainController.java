9723ff7e8015634e90e35fdbab9d9e50621b20d3
skskdmsdl <skskdmsdl@gmail.com>
2022-05-13 22:18:05 +0900

    로그인 방식 변경 중

--- src/main/java/com/project/solr/controller/MainController.java
+++ src/main/java/com/project/solr/controller/MainController.java
@@ -13,24 +13,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
-	@Autowired
-	private GoogleConnectionFactory googleConnectionFactory;
 
-	@Autowired
-	private OAuth2Parameters googleOAuth2Parameters;
+	/*
+	 * @Autowired private GoogleConnectionFactory googleConnectionFactory;
+	 * 
+	 * @Autowired private OAuth2Parameters googleOAuth2Parameters;
+	 */
 
 	@RequestMapping("/")
 	public String Main(Model model, HttpSession session) throws Exception {
-		
+
 		/* 구글code 발행 */
-	  	OAuth2Operations oauthOperations = googleConnectionFactory.getOAuthOperations();
-
-	    /* 로그인페이지 이동 url생성 */
-	  	String url = oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE, googleOAuth2Parameters);
-
-	  	model.addAttribute("google_url", url);
-	  	
+		/*
+		 * OAuth2Operations oauthOperations =
+		 * googleConnectionFactory.getOAuthOperations();
+		 * 
+		 * 로그인페이지 이동 url생성 String url =
+		 * oauthOperations.buildAuthorizeUrl(GrantType.AUTHORIZATION_CODE,
+		 * googleOAuth2Parameters);
+		 * 
+		 * model.addAttribute("google_url", url);
+		 */
 		return "main";
 	}
 
