5a4b852f819e322ad234af03e9a6c0383e5f4737
skskdmsdl <skskdmsdl@gmail.com>
2022-06-03 22:56:40 +0900

    일기 조회하기 구현 중

--- src/main/java/com/project/solr/controller/MainController.java
+++ src/main/java/com/project/solr/controller/MainController.java
@@ -3,17 +3,33 @@ package com.project.solr.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.project.solr.entity.DiaryEntity;
+import com.project.solr.repository.DiaryImageRepository;
+import com.project.solr.repository.DiaryRepository;
+import com.project.solr.service.DiaryService;
+
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	private DiaryRepository dr;
+	
+	@Autowired
+	private DiaryImageRepository dir;
+	
+	@Autowired
+    private DiaryService diaryService;
+	
 	@RequestMapping("/")
 	public String Main(HttpServletRequest request, HttpSession session) throws Exception {
 		
-		session = request.getSession();
+		//session = request.getSession();
 
+		// diary 정보 전체 가져오기
 		return "/main";
 	}
 
